e83c5cbd6d5e07723c2db8b89d7adcb504af578e
Orbitoly <orvirt@gmail.com>
2023-03-29 21:39:52 +0300

    started split

--- Task2/code.py
+++ Task2/code.py
@@ -5,45 +5,52 @@ import matplotlib.pyplot as plt
 def target(x, A, b):
     return A.T @ A @ x - A.T @ b
 
+def calc_error(x, A, b):
+    return np.linalg.norm(A @ x - b) ** 2
 
 def gradient_descent(A, x0, b, epsilon=0.1, delta=1e-5):
-    calc_error = lambda x: np.linalg.norm(A @ x - b) ** 2
 
     errors = np.array([])
     x = x0
-    errors = np.append(errors, calc_error(x))
+    errors = np.append(errors, calc_error(x, A, b))
 
     while 2 * (np.linalg.norm(target(x, A, b)) ** 2) > delta:
-        print(x)
         x = x - 2 * epsilon * target(x, A, b)
-        errors = np.append(errors, calc_error(x))
+        errors = np.append(errors, calc_error(x, A, b))
 
     return x, errors
 
 
 diabetes_X, diabetes_y = datasets.load_diabetes(return_X_y=True)
-# split randomly using numpy
 
-A = diabetes_X#np.eye(2)
-b = diabetes_y#np.array([1, 1])
+A = diabetes_X
+b = diabetes_y
 # x0 = #np.array([0.5, 0.5])
 x0 = np.ones(A.shape[1])
 x, errors = gradient_descent(A, x0, b, epsilon=0.1, delta=1e-5)
-print(x)
+
 #plot errors and show
-plt.plot(errors[:100])
-plt.xlabel('Iteration')
-plt.ylabel('Error')
-plt.title('Task 1: Error vs Iteration')
+#plt.plot(errors[:100])
+#plt.xlabel('Iteration')
+#plt.ylabel('Error')
+#plt.title('Task 1: Error vs Iteration')
 
-plt.show()
-
-#split data 80 20
+#plt.show()
 
 
-# indices = np.random.permutation(len(diabetes_X))
-# diabetes_X_train = diabetes_X[indices[:-20]]
-# diabetes_y_train = diabetes_y[indices[:-20]]
-# diabetes_X_test = diabetes_X[indices[-20:]]
-# diabetes_y_test = diabetes_y[indices[-20:]]
-# add intercept
+def split_data(X, y, ratio = 0.2):
+    indices = np.random.permutation(len(X))
+    X_train = X[indices[:-int(ratio * len(X))]]
+    y_train = y[indices[:-int(ratio * len(X))]]
+    X_test = X[indices[-int(ratio * len(X)):]]
+    y_test = y[indices[-int(ratio * len(X)):]]
+    return X_train, y_train, X_test, y_test
+
+x0 = np.ones(A.shape[1])
+X_train, y_train, X_test, y_test = split_data(diabetes_X, diabetes_y, ratio = 0.2)
+
+x, errors = gradient_descent(X_train, x0, y_train, epsilon=0.1, delta=1e-5)
+
+print(x)
+test_errors = calc_error(x, X_test, y_test)
+print(test_errors)
